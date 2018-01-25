851ea6ae069707926fab1c7e2ed741fdd80958aa
Yifei Zheng <yifeizheng43@gmail.com>
2018-01-25 11:22:43 -0500

    get rid of antlr4 inheritance

--- frontends/cxx/exprVisitor.h
+++ frontends/cxx/exprVisitor.h
@@ -1,86 +1,96 @@
+/* Copyright 2017-2018 by Yifei Zheng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// Generated from expr.g4 by ANTLR 4.7.1
-
-#pragma once
-
-
-#include "antlr4-runtime.h"
+#ifndef EXPR_VISITOR_HPP
+#define EXPR_VISITOR_HPP
+#include <antlr4-runtime.h>
 #include "exprParser.h"
 #include <support/Expr.hpp>
 
-
-
-/**
- * This class defines an abstract visitor for a parse tree
- * produced by exprParser.
- */
-class  exprVisitor : public antlr4::tree::AbstractParseTreeVisitor {
+// We don't return anything, but it's required for antlr4
+class exprVisitor {
 public:
     support::Expr expr;
-  /**
-   * Visit parse trees produced by exprParser.
-   */
-    virtual antlrcpp::Any visitUnit(exprParser::UnitContext *ctx) {
+    int visitUnit(exprParser::UnitContext* ctx) {
         if (ctx->Identifier() || ctx->Number()) {
             std::string* data = new std::string(ctx->children[0]->getText());
             expr = support::Expr();
             expr.data = reinterpret_cast<const char*>(data);
         }
         else if (ctx->functionExpression())
-            ctx->children[0]->accept(this);
+            visitFunctionExpression(static_cast<exprParser::FunctionExpressionContext*>(ctx->children[0]));
         else
-            ctx->children[1]->accept(this);
-        return nullptr;
+            visitExpression(static_cast<exprParser::ExpressionContext*>(ctx->children[1]));
     }
 
-    virtual antlrcpp::Any visitFunctionExpression(exprParser::FunctionExpressionContext *ctx) {}
+    int visitFunctionExpression(exprParser::FunctionExpressionContext* ctx) {
+        std::string* name = new std::string(ctx->children[0]->getText());
+        // minus 3 for function name, lparen, rparen, plus one for pairing up
+        // the last argument with comma
+        std::size_t sz = (ctx->children.size() - 3 + 1) / 2;
+        support::Expr root(new support::Expr::Op(sz, name));
+        auto it = ctx->children.cbegin() + 2;
+        for (std::size_t i = 0; i != sz; ++i, it += 2) {
+            visitExpression(static_cast<exprParser::ExpressionContext *>(*it));
+            root.op->args[i] = std::move(expr);
+        }
+    }
 
-    virtual antlrcpp::Any visitPowExpression(exprParser::PowExpressionContext *ctx) {
+    int visitPowExpression(exprParser::PowExpressionContext* ctx) {
         if (ctx->children.size() == 3) {
             std::string* data = new std::string(ctx->children[1]->getText());
             support::Expr root(new support::Expr::Op(2, data));
-            ctx->children[0]->accept(this);
+            visitPowExpression(static_cast<exprParser::PowExpressionContext*>(ctx->children[0]));
             root.op->args[0] = std::move(expr);
-            ctx->children[2]->accept(this);
+            visitUnit(static_cast<exprParser::UnitContext*>(ctx->children[2]));
             root.op->args[1] = std::move(expr);
             expr = std::move(root);
         }
         else {
-            ctx->children[0]->accept(this);
+            visitUnit(static_cast<exprParser::UnitContext*>(ctx->children[0]));
         }
-        return nullptr;
     }
 
-    virtual antlrcpp::Any visitMultiplicativeExpression(exprParser::MultiplicativeExpressionContext *ctx) {
+    int visitMultiplicativeExpression(exprParser::MultiplicativeExpressionContext* ctx) {
         if (ctx->children.size() == 3) {
             std::string* data = new std::string(ctx->children[1]->getText());
             support::Expr root(new support::Expr::Op(2, data));
-            ctx->children[0]->accept(this);
+            visitMultiplicativeExpression(static_cast<exprParser::MultiplicativeExpressionContext*>(ctx->children[0]));
             root.op->args[0] = std::move(expr);
-            ctx->children[2]->accept(this);
+            visitPowExpression(static_cast<exprParser::PowExpressionContext*>(ctx->children[2]));
             root.op->args[1] = std::move(expr);
             expr = std::move(root);
         }
         else {
-            ctx->children[0]->accept(this);
+            visitPowExpression(static_cast<exprParser::PowExpressionContext*>(ctx->children[0]));
         }
-        return nullptr;
     }
 
-    virtual antlrcpp::Any visitExpression(exprParser::ExpressionContext *ctx) {
+    int visitExpression(exprParser::ExpressionContext* ctx) {
         if (ctx->children.size() == 3) {
             std::string* data = new std::string(ctx->children[1]->getText());
             support::Expr root(new support::Expr::Op(2, data));
-            ctx->children[0]->accept(this);
+            visitExpression(static_cast<exprParser::ExpressionContext*>(ctx->children[0]));
             root.op->args[0] = std::move(expr);
-            ctx->children[2]->accept(this);
+            visitMultiplicativeExpression(static_cast<exprParser::MultiplicativeExpressionContext*>(ctx->children[2]));
             root.op->args[1] = std::move(expr);
             expr = std::move(root);
         }
         else {
-            ctx->children[0]->accept(this);
+            visitMultiplicativeExpression(static_cast<exprParser::MultiplicativeExpressionContext*>(ctx->children[0]));
         }
-        return nullptr;
     }
 };
-
+#endif
